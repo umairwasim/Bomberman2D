20891a0230c58ebf4332d182c1c289d3b1cfec42
Umair <umairwasim001@gmail.com>
2023-07-07 04:34:47 +0500

    Add items spawn logic

--- Bomberman2D/Assets/Scripts/Destructible.cs
+++ Bomberman2D/Assets/Scripts/Destructible.cs
@@ -4,8 +4,21 @@ public class Destructible : MonoBehaviour
 {
     public float destructionTime = 1f;
 
+    [Range(0f, 1f)]
+    public float itemSpawnChance = 0.2f;
+
+    [SerializeField] private GameObject[] items;
+
     void Start()
     {
         Destroy(gameObject, destructionTime);
+    }
+
+    private void OnDestroy()
+    {
+        if (items.Length > 0 && Random.value < itemSpawnChance)
+        {
+            Instantiate(items[Random.Range(0, items.Length)], transform.position, Quaternion.identity);
+        }
     }
 }
